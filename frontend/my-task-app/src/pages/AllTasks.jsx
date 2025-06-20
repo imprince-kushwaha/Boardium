68866ea4a4aff36745cae8ea732514b6e99973a9
imprince_kushwaha <princeking19ald@gmail.com>
2025-06-20 16:44:57 +0530

    Admin All Task Page

---
+++ frontend/my-task-app/src/pages/AllTasks.jsx
@@ -0,0 +1,45 @@
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import React, { useState } from "react";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const AllTasks = () => {
+  const [search, setSearch] = useState("");
+
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <div className="m-4 ml-20 sm:ml-60">
+        <div className="flex gap-8 justify-between">
+          <div className="text-4xl">Tasks</div>
+          <div>
+            <Button label="+ Add New Task" outlined />
+          </div>
+        </div>
+
+        <div className="flex justify-end mt-2 mb-2">
+          <InputText
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search Tasks"
+            className="w-2xs"
+          />
+        </div>
+
+        <div className="grid grid-cols-4 font-semibold text-gray-600 border-b pb-2">
+          <div>Task Name</div>
+          <div>Assigned To</div>
+          <div>Due Date</div>
+          <div>Status</div>
+        </div>
+        {/* Here the content should be scrollable */}
+      </div>
+      <Footer/>
+    </>
+  );
+};
+
+export default AllTasks;
