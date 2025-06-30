3c88380f5eb9f3243070c870060d20d2b1f64b6a
imprince_kushwaha <princeking19ald@gmail.com>
2025-06-30 17:09:10 +0530

    Added path to sidebarItems

--- frontend/my-task-app/src/pages/Sidebar.jsx
+++ frontend/my-task-app/src/pages/Sidebar.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 import { MdHome } from "react-icons/md";
 import { FaFolderOpen } from "react-icons/fa";
 import { BsListTask } from "react-icons/bs";
 import { IoMdPerson } from "react-icons/io";
 
 const Sidebar = () => {
- const sidebarItems = [
-    { name: 'Home', icon: <MdHome /> },
-    { name: 'Projects', icon: <FaFolderOpen /> },
-    { name: 'Tasks', icon: <BsListTask /> },
-    { name: 'Team Management', icon: <IoMdPerson /> },
+  const sidebarItems = [
+    { name: "Home", icon: <MdHome />, path: "/home" },
+    { name: "Projects", icon: <FaFolderOpen />, path: "/projects" },
+    { name: "Tasks", icon: <BsListTask />, path: "/tasks" },
+    { name: "Team Management", icon: <IoMdPerson />, path: "/team" },
   ];
+
   return (
     <div className="w-16 sm:w-56 h-full fixed bg-white text-gray-700 shadow-md">
       <div className="p-6 text-2xl font-bold hidden sm:block">
@@ -18,23 +20,31 @@ const Sidebar = () => {
         Boardium
       </div>
       <div className="p-4 text-2xl font-bold sm:hidden text-center">
-        {/* sm and above sm it will be hidden */}
-        B
+        {/* sm and above sm it will be hidden */}B
       </div>
 
       <nav className="flex flex-col space-y-2 mt-4">
         {sidebarItems.map((item, index) => (
-          <div
+          <NavLink
             key={index}
-            className="flex items-center justify-center sm:justify-start space-x-0 sm:space-x-3 px-4 py-3 hover:bg-gray-200 cursor-pointer transition-all"
+            to={item.path}
+            className={({ isActive }) =>
+              `flex items-center justify-center sm:justify-start space-x-0 sm:space-x-3 px-4 py-3 transition-all cursor-pointer ${
+                isActive
+                  ? "bg-gray-300 font-semibold text-blue-600"
+                  : "hover:bg-gray-200"
+              }`
+            }
           >
             <span className="text-xl">{item.icon}</span>
-            <span className="text-md font-medium hidden sm:inline">{item.name}</span>
-          </div>
+            <span className="text-md font-medium hidden sm:inline">
+              {item.name}
+            </span>
+          </NavLink>
         ))}
       </nav>
     </div>
   );
-}
+};
 
-export default Sidebar
+export default Sidebar;
