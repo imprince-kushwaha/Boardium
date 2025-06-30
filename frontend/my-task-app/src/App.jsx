d8a0affddb786f324f51ac0fbca14684058b8d00
imprince_kushwaha <princeking19ald@gmail.com>
2025-06-30 17:02:30 +0530

    Used react-router-dom

--- frontend/my-task-app/src/App.jsx
+++ frontend/my-task-app/src/App.jsx
@@ -1,4 +1,10 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import KanbanBoard from "./components/KanbanBoard";
 import Login from "./components/Login";
 import MainPage from "./components/Admin/AdminMainPage";
@@ -8,21 +14,41 @@ import AllTasks from "./pages/AllTasks";
 import Header from "./pages/Header";
 import Sidebar from "./pages/Sidebar";
 import UserMainPage from "./components/User/UserMainPage";
+import AddProject from "./components/Admin/AddProject";
+import UserManagement from "./components/Admin/UserManagement";
 
 function App() {
   return (
-    <>     
+    <>
       {/* <KanbanBoard /> */}
       {/* <Kendo/>  */}
 
       {/* <Header />
       <Sidebar /> */}
       {/* <MainPage/> */}
-      <UserMainPage/>
-      {/* <AllTasks/> */}
-      {/* <Register/>
-      <Login/> */}
+      {/* main page */}
+      {/* <UserMainPage/> */}
 
+      {/* <AllTasks/> */}
+      {/* <AddProject/> */}
+
+      {/* <Router>
+        <Routes>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+      </Router> */}
+
+      <Router>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<UserMainPage />} />
+          <Route path="/projects" element={<AddProject />} />
+          <Route path="/team" element={<UserManagement />} />
+          {/* {/* <Route path="/" element={<Navigate to="/login" />} />  */}
+        </Routes>
+      </Router>
     </>
   );
 }
