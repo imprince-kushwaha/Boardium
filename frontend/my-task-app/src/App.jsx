68e1ec44ac80c8c0cbe861301c5a41691b3105bc
imprince_kushwaha <princeking19ald@gmail.com>
2025-07-07 17:53:53 +0530

    Changes in App,jsx and index.css files

--- frontend/my-task-app/src/App.jsx
+++ frontend/my-task-app/src/App.jsx
@@ -5,35 +5,21 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import KanbanBoard from "./components/KanbanBoard";
 import Login from "./components/Login";
-import MainPage from "./components/Admin/AdminMainPage";
 import Register from "./components/Register";
-import Kendo from "./KendoKanban/Kendo";
 import AllTasks from "./pages/AllTasks";
-import Header from "./pages/Header";
-import Sidebar from "./pages/Sidebar";
 import UserMainPage from "./components/User/UserMainPage";
 import AddProject from "./components/Admin/AddProject";
 import UserManagement from "./components/Admin/UserManagement";
+import AdminMainPage from "./components/Admin/AdminMainPage";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import MySettings from "./pages/MySettings";
-import CommonLayout from "./pages/CommonLayout";
+import ProjectKanban from "./components/User/ProjectKanban";
 
 function App() {
   return (
     <>
-      {/* <KanbanBoard /> */}
-      {/* <Kendo/>  */}
-
-      {/* <MainPage/> */}
-      
-      {/* <UserMainPage/> */}
-
-      {/* <AllTasks/> */}
-      {/* <AddProject/> */}
-
       <Router>
         <Routes>
           <Route path="/register" element={<Register />} />
@@ -41,13 +27,13 @@ function App() {
           <Route path="/home" element={<UserMainPage />} />
           <Route path="/projects" element={<AddProject />} />
           <Route path="/team" element={<UserManagement />} />
-          <Route path="/projects/kanban/:projectId" element={<KanbanBoard />} />
+          <Route path="/projects/kanban/:projectId" element={<ProjectKanban />} />
           <Route path="/settings" element={<MySettings />} />
-          {/* <Route path="/common" element={< />} /> */}
-          <Route path="/" element={<Navigate to="/login" />} /> 
+          <Route path="/tasks" element={<AllTasks />} />
+          <Route path="/" element={<Navigate to="/login" />} />
         </Routes>
       </Router>
-      <ToastContainer position="top-right"/>
+      <ToastContainer position="top-right" />
     </>
   );
 }
